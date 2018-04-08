ef0f007c2115e363ae6ccc1d0f900867fd01d954
Ben Hyrman <ben.hyrman@gmail.com>
2018-04-08 23:02:25 +0200

    Added integration test example

---
+++ src/test/java/com/boot/ShipwreckRepositoryIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.boot;
+
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+@WebAppConfiguration
+public class ShipwreckRepositoryIntegrationTest {
+    @Autowired
+    private ShipwreckRepository shipwreckRepository;
+    @Test
+    public void testFindAll() {
+        List<Shipwreck> wrecks = shipwreckRepository.findAll();
+        assertThat(wrecks.size(), is(greaterThanOrEqualTo(0)));
+    }
+}
